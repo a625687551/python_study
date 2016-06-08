d4400a5d50007d96cedbf9e267532f0b491ab397
a625687551 <625687551@qq.com>
2016-06-08 15:17:12 +0800

    study

--- recursive function.py
+++ recursive function.py
@@ -5,9 +5,18 @@ Created on Wed Jun  8 14:31:21 2016
 @author: wangjianfeng2014
 """
 #recursive function
+'''
 def fact(n):
     if(n==1):
         return 1
     return n*fact(n-1)
 print(fact(1))
 print(fact(1000))
+'''
+def fact(n):
+    return fact_iter(n,1)
+def fact_iter(num,product):
+    if num==1:
+        return product
+    return fact_iter(num-1,num*product)
+print(fact(100))
