f3b93b86cbcb9f64640292dc9c354a562aea1891
a625687551 <625687551@qq.com>
2016-06-24 17:35:45 +0800

    change

--- account_login.py
+++ account_login.py
@@ -6,18 +6,25 @@
 _author_='wangjianfeng'
 
 password_list=['****','12345']
+
 def account_login():
-    password=input('please input your password:')
-    password_correct=password==password_list[-1]
-    password_reset=password==password_list[0]
-    if password_correct:
-        print('login success')
-    elif password_reset:
-        new_password=input('enter a new password:')
-        password_list.append(new_password)
-        print('your password has changed successfully')
-        account_login()
+    tries=3
+    while tries>0:
+        password=input('please input your password:')
+        password_correct=password==password_list[-1]
+        password_reset=password==password_list[0]
+        if password_correct:
+            print('login success!')
+        elif password_reset:
+            new_password=input('enter your new password:')
+            password_list.append(new_password)
+            print('change your password successfully!')
+            account_login()
+        else:
+            print('wrong password or invalid input!')
+            tries=tries-1
+            print(tries,'time left')
     else:
-        print('wrong password or invalid input')
-        account_login()
+        print('your account has been suspended')
 account_login()
+            
